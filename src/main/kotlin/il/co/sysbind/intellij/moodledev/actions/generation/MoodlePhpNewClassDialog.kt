12754670d41fc226ea4b33a6e8015d49e61fab1e
Avi Levy <avi@sysbind.co.il>
2022-05-09 14:41:52 +0300

    moodle-dev-35 make different new file action in later work

--- src/main/kotlin/il/co/sysbind/intellij/moodledev/actions/generation/MoodlePhpNewClassDialog.kt
+++ src/main/kotlin/il/co/sysbind/intellij/moodledev/actions/generation/MoodlePhpNewClassDialog.kt
@@ -1,13 +1,19 @@
 package il.co.sysbind.intellij.moodledev.actions.generation
 
-import com.intellij.openapi.project.DumbService
+import com.intellij.ide.fileTemplates.FileTemplateManager
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.text.StringUtil
 import com.intellij.psi.PsiDirectory
+import com.intellij.ui.EditorTextField
 import com.jetbrains.php.actions.PhpNewClassDialog
 import com.jetbrains.php.actions.newClassDataProvider.ClassCreationType
+import com.jetbrains.php.lang.PhpLangUtil
+import com.jetbrains.php.templates.PhpFileTemplateUtil
+import java.util.*
 
 class MoodlePhpNewClassDialog(project: Project, directory: PsiDirectory) : PhpNewClassDialog(project, directory) {
 
+    private val mySuperFqnTextField = EditorTextField()
     override fun init() {
         super.init()
         var template = ClassCreationType.BUNDLED.iterator()
@@ -30,15 +36,30 @@ class MoodlePhpNewClassDialog(project: Project, directory: PsiDirectory) : PhpNe
             "Trait", com.jetbrains.php.PhpIcons.TRAIT, "Moodle PHP Trait"
             )
         )
+        updateSuperClassesPanel()
     }
 
-    override fun canInheritSuperClasses(): Boolean {
-        return if (DumbService.isDumb(myProject)) {
-            false
-        } else {
-            val selectedClassCreationType = this.selectedClassCreationType
-            canOverrideSuperClass(selectedClassCreationType) || canImplementInterface(selectedClassCreationType) ||
-                    selectedClassCreationType.templateName == "Moodle PHP Class" || selectedClassCreationType.templateName == "Moodle PHP Interface"
-        }
+    private fun updateSuperClassesPanel() {
+        mySuperFqnTextField.isEnabled = true
+    }
+
+    private fun getIgnoredProperties(): Properties {
+        val properties = Properties()
+        properties.setProperty("NAME", className)
+        properties.setProperty("NAMESPACE", namespaceName)
+        properties.setProperty("FILE_NAME", fileName)
+        properties.setProperty("CARET", "")
+        PhpFileTemplateUtil.fillDefaultProperties(
+            FileTemplateManager.getInstance(myProject),
+            properties,
+            baseDirectory
+        )
+
+        return properties
+    }
+
+    override fun getSuperFqn(): String? {
+        val superFqn = mySuperFqnTextField.text
+        return if (StringUtil.isNotEmpty(superFqn)) PhpLangUtil.toFQN(superFqn!!) else null
     }
 }
