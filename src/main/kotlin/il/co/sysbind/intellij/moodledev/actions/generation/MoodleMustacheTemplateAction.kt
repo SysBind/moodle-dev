256bb4a197a9ec16992dcf3d200c21843ce648b3
Avi Levy <avi@sysbind.co.il>
2023-12-15 00:26:30 +0200

    Add MoodleMustacheTemplateAction and update template creation logic
    Introduced MoodleMustacheTemplateAction class to enable creation of Mustache templates. Streamlined template properties logic in PhpCreateFileAction by introducing MoodleCorePathUtil::getPluginName method. Also updated plugin dependencies to include Handlebars plugin.

--- src/main/kotlin/il/co/sysbind/intellij/moodledev/actions/generation/MoodleMustacheTemplateAction.kt
+++ src/main/kotlin/il/co/sysbind/intellij/moodledev/actions/generation/MoodleMustacheTemplateAction.kt
@@ -11,11 +11,12 @@ import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiDirectory
 import com.intellij.psi.PsiFile
-import com.jetbrains.php.PhpIcons
+import icons.HandlebarsIcons
 import il.co.sysbind.intellij.moodledev.project.MoodleProjectSettings
 import il.co.sysbind.intellij.moodledev.util.MoodleCorePathUtil
 
-class PhpCreateFileAction : CreateFileFromTemplateAction(CAPTION, "", PhpIcons.PHP_FILE), DumbAware {
+class MoodleMustacheTemplateAction : CreateFileFromTemplateAction(CAPTION, "", HandlebarsIcons.Handlebars_icon),
+    DumbAware {
     override fun getActionName(directory: PsiDirectory?, newName: String, templateName: String?): String = CAPTION
 
     override fun isAvailable(dataContext: DataContext?): Boolean {
@@ -25,9 +26,10 @@ class PhpCreateFileAction : CreateFileFromTemplateAction(CAPTION, "", PhpIcons.P
         val moodle = project.getService(MoodleProjectSettings::class.java).settings
         return moodle.pluginEnabled
     }
+
     override fun buildDialog(project: Project, directory: PsiDirectory, builder: CreateFileFromTemplateDialog.Builder) {
-        builder.setTitle(CAPTION)
-            .addKind("Empty file", PhpIcons.PHP_FILE, "Moodle PHP File")
+        builder.setTitle(PhpCreateFileAction.CAPTION)
+            .addKind("Empty file", HandlebarsIcons.Handlebars_icon, "Moodle Mustache Template")
     }
 
     override fun createFileFromTemplate(name: String, template: FileTemplate, dir: PsiDirectory): PsiFile {
@@ -37,8 +39,6 @@ class PhpCreateFileAction : CreateFileFromTemplateAction(CAPTION, "", PhpIcons.P
         // Create properties for the template
         val templateManager = FileTemplateManager.getInstance(project)
         val props = templateManager.defaultProperties
-        props["USER_NAME"] = settings.userName
-        props["USER_EMAIL"] = settings.userEmail
 
         // Extract PLUGIN_NAME from the closest version.php
         props["PLUGIN_NAME"] = MoodleCorePathUtil.getPluginName(dir)
@@ -46,7 +46,8 @@ class PhpCreateFileAction : CreateFileFromTemplateAction(CAPTION, "", PhpIcons.P
         return FileTemplateUtil.createFromTemplate(template, name, props, dir).containingFile
     }
 
-    companion object {
-        internal const val CAPTION = "Moodle PHP File"
+    private companion object {
+        private const val CAPTION = "Moodle Mustache Template"
     }
+
 }
--- src/main/kotlin/il/co/sysbind/intellij/moodledev/util/MoodleCorePathUtil.kt
+++ src/main/kotlin/il/co/sysbind/intellij/moodledev/util/MoodleCorePathUtil.kt
@@ -3,7 +3,10 @@ package il.co.sysbind.intellij.moodledev.util
 import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiDirectory
 import il.co.sysbind.intellij.moodledev.moodle.Component
+import java.nio.file.FileSystems
+import java.nio.file.Files
 
 object MoodleCorePathUtil {
 
@@ -32,9 +35,11 @@ object MoodleCorePathUtil {
         return null
     }
 
-    fun findFileUpwards(startDir: VirtualFile, filename: String): VirtualFile? {
-        var dir: VirtualFile? = startDir
-        while (dir != null) {
+    fun findFileUpwards(startDir: PsiDirectory, filename: String): VirtualFile? {
+        var dir: VirtualFile = startDir.virtualFile
+        val project = startDir.project
+        while (dir.path.startsWith(project.basePath.toString())) {
+            // stop loop if dir is not under baseDir
             val file = VfsUtil.findRelativeFile(dir, filename)
             if (file != null) {
                 return file
@@ -42,5 +47,19 @@ object MoodleCorePathUtil {
             dir = dir.parent
         }
         return null
+    }
+
+    fun getPluginName(startDir: PsiDirectory): String {
+        val versionFilePath = findFileUpwards(startDir, "version.php")?.path
+        if (versionFilePath != null && Files.exists(FileSystems.getDefault().getPath(versionFilePath))) {
+            val componentLine = Files.readAllLines(FileSystems.getDefault().getPath(versionFilePath))
+                .firstOrNull { it.trim().startsWith("\$plugin->component") }
+            if (componentLine != null) {
+                val pluginName = componentLine.split("=")[1].trim().removeSuffix(";").removeSurrounding("\"", "\"")
+                    .removeSurrounding("\'", "\'")
+                return pluginName
+            }
+        }
+        return ""
     }
 }
