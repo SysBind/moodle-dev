bf7fde97454c13eb44f7322f174d23807cef0f59
Avi Levy <avi@sysbind.co.il>
2025-08-12 01:08:19 +0300

    Update workflows, dependencies, and configurations:
    - Free up disk space in CI workflows using jlumbroso/free-disk-space@v1.3.1.
    - Enable read-only Gradle caching and adjust task dependencies.
    - Add `codecov.yml` for coverage reporting.
    - Update Qodana linter version and JDK to 21 in `qodana.yml`.
    - Remove `pluginUntilBuild` and introduce `platformBundledModules` property in Gradle.
    - Update plugin metadata in `plugin.xml` to version 2.1.0 and add resource-bundle entry.
    - Refactor release asset creation logic to streamline handling of changelogs and releases.
    - Fix licensing comments and adjust Gradle wrapper scripts.
    - Ignore `.DS_Store` and minor content fixes in `.gitignore`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ repositories {
 // Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
 dependencies {
     testImplementation(libs.junit)
+    testImplementation(libs.opentest4j)
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
@@ -43,9 +44,9 @@ dependencies {
         // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file for plugin from JetBrains Marketplace.
         plugins(providers.gradleProperty("platformPlugins").map { it.split(',') })
 
-        instrumentationTools()
-        pluginVerifier()
-        zipSigner()
+        // Module Dependencies. Uses `platformBundledModules` property from the gradle.properties file for bundled IntelliJ Platform modules.
+        bundledModules(providers.gradleProperty("platformBundledModules").map { it.split(',') })
+
         testFramework(TestFrameworkType.Platform)
     }
 }
@@ -53,6 +54,7 @@ dependencies {
 // Configure IntelliJ Platform Gradle Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-extension.html
 intellijPlatform {
     pluginConfiguration {
+        name = providers.gradleProperty("pluginName")
         version = providers.gradleProperty("pluginVersion")
 
         // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
@@ -83,7 +85,6 @@ intellijPlatform {
 
         ideaVersion {
             sinceBuild = providers.gradleProperty("pluginSinceBuild")
-            untilBuild = providers.gradleProperty("pluginUntilBuild")
         }
     }
 
@@ -102,7 +103,6 @@ intellijPlatform {
     }
 
     pluginVerification {
-        freeArgs = providers.gradleProperty("pluginVerifierArgs").map { it.split(' ') }
         ides {
             recommended()
         }
